c93587203972867ed2e02d9259255ed00f3707d7
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-28 13:32:51 +0530

    post section

--- src/components/common/Posts.jsx
+++ src/components/common/Posts.jsx
@@ -1,28 +1,77 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "./../../utils/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+const Posts = ({ feedType }) => {
+  const getPostEndpoint = () => {
+    switch (feedType) {
+      case "forYou":
+        return "http://localhost:10000/api/posts/all";
+      case "following":
+        return "http://localhost:10000/api/posts/following";
+      case "posts":
+        return `http://localhost:10000/api/posts/user/${username}`;
+      case "likes":
+        return `http://localhost:10000/api/posts/likes/${userId}`;
+      default:
+        return "http://localhost:10000/api/posts/all";
+    }
+  };
+  const POST_ENDPOINT = getPostEndpoint();
 
-const Posts = () => {
-	const isLoading = false;
+  const {
+    data: posts,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        const data = await res.json();
 
-	return (
-		<>
-			{isLoading && (
-				<div className='flex flex-col justify-center'>
-					<PostSkeleton />
-					<PostSkeleton />
-					<PostSkeleton />
-				</div>
-			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
-				<div>
-					{POSTS.map((post) => (
-						<Post key={post._id} post={post} />
-					))}
-				</div>
-			)}
-		</>
-	);
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+  
+  useEffect(() => {
+	refetch();
+}, [feedType, refetch]);
+
+return (
+	<>
+		{(isLoading || isRefetching) && (
+			<div className='flex flex-col justify-center'>
+				<PostSkeleton />
+				<PostSkeleton />
+				<PostSkeleton />
+			</div>
+		)}
+		{!isLoading && !isRefetching && posts?.length === 0 && (
+			<p className='text-center my-4'>No posts in this tab. Switch 👻</p>
+		)}
+		{!isLoading && !isRefetching && posts && (
+			<div>
+				{posts.map((post) => (
+					<Post key={post._id} post={post} />
+				))}
+			</div>
+		)}
+	</>
+);
 };
 export default Posts;
--- src/components/common/RightPanel.jsx
+++ src/components/common/RightPanel.jsx
@@ -1,9 +1,37 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/dummy";
+import { useQuery } from "@tanstack/react-query";
+import useFollow from "../../hooks/useFollow";
+import LoadingSpinner from "./LoadingSpinner";
+
 
 const RightPanel = () => {
-	const isLoading = false;
+	const { data: suggestedUsers, isLoading } = useQuery({
+		queryKey: ["suggestedUsers"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("http://localhost:10000/api/user/sugessted",{
+					method: "GET",
+					headers: {
+					  "Content-Type": "application/json",
+					},
+					credentials: "include",
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong!");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error.message);
+			}
+		},
+	});
+
+	const {follow, isPending}=useFollow();
+	console.log("suggested user ",suggestedUsers);
+
+	if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -20,7 +48,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUsers?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
@@ -42,9 +70,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) => {
+											e.preventDefault();
+											follow(user._id);
+										}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size='sm' /> : "Follow"}
 									</button>
 								</div>
 							</Link>
