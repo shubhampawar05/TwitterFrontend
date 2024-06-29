3c223da874e1aea90521be79ecd799ed027c0ba8
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-29 12:26:16 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-	  "./src/**/*.{js,jsx,ts,tsx}",
-	],
-	theme: {
-	  extend: {},
-	},
-	plugins: [],
-  }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
